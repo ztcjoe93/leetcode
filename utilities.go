c5df18a61b7f088d983139392ef0b2bde9c2c2bf
joe@nastrond <ztcjoe93@gmail.com>
2022-11-23 21:34:29 +0800

    feat: added fullEval function to get collated results

--- utilities.go
+++ utilities.go
@@ -2,12 +2,25 @@ package main
 
 import "fmt"
 
-func eval(inputs interface{}, expected interface{}, outputs interface{}) {
+func fullEval(results ...bool) {
+	passed := 0
+	for _, result := range results {
+		if result {
+			passed++
+		}
+	}
+
+	fmt.Printf("%v/%v TEST CASES PASSED\n", passed, len(results))
+}
+
+func eval(inputs interface{}, expected interface{}, outputs interface{}) bool {
 	fmt.Printf("inputs=%v,expected=%v,outputs=%v\n", inputs, expected, outputs)
 
 	if outputs == expected {
 		fmt.Printf("TEST CASE PASSED\n\n")
+		return true
 	} else {
 		fmt.Printf("TEST CASE FAILED\n\n")
+		return false
 	}
 }
